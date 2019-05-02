37d2d34d1443360c9b793328eb491a2da4f6150b
abal0011 <aravind.cyber@yahoo.co.in>
2019-05-02 13:00:26 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -106,9 +106,21 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+<ol>
+{profiles.map(profile => 
+ { 			const userName = users[profile.userID].name;
+            const favMovieName = movies[profile.favoriteMovieID].name;
+return(
+<li key={profile.id}>
+
+ <p>{`${userName}'s favorite movie is ${favMovieName}`}</p>);
+</li>
+ );
+})}
+</ol>
       </div>
-    );
-  }
+   );
+}
 }
 
 export default App;
